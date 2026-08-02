4d94deae62e7ff6b339cb72b5995022a300a8076
kailash <kailashwebdeveloper@gmail.com>
2026-08-02 11:22:20 +0530

    initial commit

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
+# Current deployment directory (where appspec.yaml is located)
+DEPLOY_DIR=$(pwd)
+
 APP_DIR=/opt/python-app
 
 mkdir -p $APP_DIR
 
-cd $APP_DIR
+cp -r "$DEPLOY_DIR"/* "$APP_DIR"/
+
+cd "$APP_DIR"
 
 python3 -m venv venv
 
@@ -14,7 +19,3 @@ source venv/bin/activate
 pip install --upgrade pip
 
 pip install -r requirements.txt
-
-cp service/python-app.service /etc/systemd/system/python-app.service
-
-systemctl daemon-reload
