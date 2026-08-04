440259adc84c31e1e7cd43050f4bcabbf4837275
kailashwebdeveloper <51291854+kailashwebdeveloper@users.noreply.github.com>
2026-08-04 09:39:10 +0530

    Copy python-app.service to systemd directory

--- scripts/install.sh
+++ scripts/install.sh
@@ -37,3 +37,5 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "===== INSTALL COMPLETE ====="
+
+cp -pvrf service/python-app.service /etc/systemd/system/python-app.service
